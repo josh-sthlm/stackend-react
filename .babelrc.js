7e914e05443414b557c85cda0dc85ab1ff7cc6d8
jens <jens@josh.se>
2021-01-22 10:46:02 +0100

    Added stackend css prefix

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,4 @@
-const { NODE_ENV } = process.env
+const { NODE_ENV } = process.env;
 
 module.exports = {
   presets: [
@@ -18,7 +18,14 @@ module.exports = {
   plugins: [
     // don't use `loose` mode here - need to copy symbols when spreading
     '@babel/proposal-object-rest-spread',
-    'babel-plugin-styled-components',
+    ['@quickbaseoss/babel-plugin-styled-components-css-namespace', { cssNamespace: 'stackend' }],
+    [
+      'babel-plugin-styled-components',
+      {
+        displayName: true,
+        ssr: true
+      }
+    ],
     NODE_ENV === 'test' && '@babel/transform-modules-commonjs'
   ].filter(Boolean)
-}
+};
