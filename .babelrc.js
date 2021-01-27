e040cc78f25a23dff58e4a89bceab0330f2ccbaa
jens <jens@josh.se>
2021-01-27 11:46:12 +0100

    Build using babel

--- .babelrc.js
+++ .babelrc.js
@@ -2,27 +2,29 @@ const { NODE_ENV } = process.env;
 
 module.exports = {
   presets: [
-    '@babel/typescript',
     [
       '@babel/env',
       {
         targets: {
-          browsers: ['ie >= 11']
+          browsers: ['ie >= 11'],
+          node: '12.19'
         },
         exclude: ['transform-async-to-generator', 'transform-regenerator'],
         modules: false,
         loose: true
       }
-    ]
+    ],
+    '@babel/preset-typescript',
+    '@babel/preset-react'
   ],
   plugins: [
     '@babel/proposal-class-properties',
-    // don't use `loose` mode here - need to copy symbols when spreading
     '@babel/proposal-object-rest-spread',
-    ['@quickbaseoss/babel-plugin-styled-components-css-namespace', { cssNamespace: 'stackend' }],
+    ['@quickbaseoss/babel-plugin-styled-components-css-namespace', { cssNamespace: '.stackend' }],
     [
       'babel-plugin-styled-components',
       {
+        minify: false,
         displayName: true,
         ssr: true
       }
