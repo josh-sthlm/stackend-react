b08c7a1f6c3a47b893809a1c1acbcec82c7ca5db
jens <jens@josh.se>
2022-03-24 15:45:08 +0100

    Work on themes

--- shop/ProductImageModal.style.ts
+++ shop/ProductImageModal.style.ts
@@ -12,16 +12,16 @@ export const ProductImageModal = styled.div.attrs(props => ({
 
   button {
     z-index: ${zIndexes.modal + 10};
+    font-size: 3rem;
+    ${media.mobileScreen} {
+      font-size: 2rem;
+    }
   }
 
   button.stackend-close {
     position: absolute;
     top: 0;
     right: 0;
-    font-size: 3rem;
-    ${media.mobileScreen} {
-      font-size: 2rem;
-    }
   }
 
   button.stackend-previous,
@@ -33,7 +33,6 @@ export const ProductImageModal = styled.div.attrs(props => ({
     background: ${props =>
       getComponentProp(props.theme, ComponentType.BUTTON, 'color') + '22' || 'rgba(255, 255, 255, 0.5)'};
     border-radius: 50%;
-    font-size: 4rem;
     position: absolute;
     &:hover,
     &:active {
